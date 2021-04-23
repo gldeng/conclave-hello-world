b7e50e590076fb73e9334bc34d48082e587fda1f
gldeng <realgldeng@gmail.com>
2021-04-23 18:09:31 +0800

    Add decryptHost

---
+++ decryptHost/src/main/java/com/r3/conclave/sample/decryptHost/Host.java
@@ -0,0 +1,84 @@
+package com.r3.conclave.sample.decryptHost;
+
+import com.r3.conclave.common.EnclaveInstanceInfo;
+import com.r3.conclave.host.AttestationParameters;
+import com.r3.conclave.host.EnclaveHost;
+import com.r3.conclave.host.EnclaveLoadException;
+import com.r3.conclave.host.MailCommand;
+import com.r3.conclave.host.MockOnlySupportedException;
+import com.r3.conclave.utilities.internal.UtilsKt;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * This class demonstrates how to load an enclave and exchange byte arrays with it.
+ */
+public class Host {
+    private static void doDecryptFLow(String hexString) throws IOException, EnclaveLoadException {
+        EnclaveHost enclave = EnclaveHost.load("com.r3.conclave.sample.enclave.MailForStorageEnclave");
+        enclave.start(new AttestationParameters.DCAP(), (commands) -> {
+            // ignore
+        });
+
+        // The attestation data must be provided to the client of the enclave, via whatever mechanism you like.
+        final EnclaveInstanceInfo attestation = enclave.getEnclaveInstanceInfo();
+        final byte[] attestationBytes = attestation.serialize();
+
+        // It has a useful toString method.
+        System.out.println(EnclaveInstanceInfo.deserialize(attestationBytes));
+
+
+        byte[] encrypted = UtilsKt.parseHex(hexString);
+
+        enclave.deliverMail(1, encrypted, "routingHint");
+
+        byte[] decrypted = enclave.callEnclave(new byte[0]);
+        System.out.println();
+        System.out.println("Decrypted: " + new String(decrypted));
+        System.out.println();
+
+        enclave.close();
+    }
+
+    private static void reportPlatformSupport() {
+        // Report whether the platform supports hardware enclaves.
+        //
+        // This method will always check the hardware state regardless of whether running in Simulation,
+        // Debug or Release mode. If the platform supports hardware enclaves then no exception is thrown.
+        // If the platform does not support enclaves or requires enabling, an exception is thrown with the
+        // details in the exception message.
+        //
+        // If the platform supports enabling of enclave support via software then passing true as a parameter
+        // to this function will attempt to enable enclave support on the platform. Normally this process
+        // will have to be run with root/admin privileges in order for it to be enabled successfully.
+        try {
+            EnclaveHost.checkPlatformSupportsEnclaves(true);
+            System.out.println("This platform supports enclaves in simulation, debug and release mode.");
+        } catch (MockOnlySupportedException e) {
+            System.out.println("This platform only supports mock enclaves: " + e.getMessage());
+            System.exit(1);
+        } catch (EnclaveLoadException e) {
+            System.out.println("This platform does not support hardware enclaves: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) throws EnclaveLoadException, IOException {
+
+        if (args.length == 0) {
+            System.err.println("Please pass the string to reverse on the command line using --args=\"String to Reverse\"");
+            return;
+        }
+
+        String hexString = String.join(" ", args);
+
+        reportPlatformSupport();
+        doDecryptFLow(hexString);
+    }
+
+}
